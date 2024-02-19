ad005f9b48243ebebea11f715ded44cc01984fc8
tobias-barakaa <ngairato@gmail.com>
2024-02-19 15:24:44 +0300

    activation pin

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -8,6 +9,12 @@ class JobPost(models.Model):
     description = models.CharField(max_length=200)
     date = models.DateTimeField(auto_now_add=True) 
     salary = models.IntegerField()
+    slug = models.SlugField(null=True)
+    
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.title)
+        return super(JobPost, self).save(*args, **kwargs)
   
     
     def __str__(self):
